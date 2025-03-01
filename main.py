17dd849865c854f42c9aff58151001458a32063d
Gregoire Saugnac <saugnacgj@cardiff.ac.uk>
2025-03-01 18:14:39 +0000

    Filter frontend working

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
@@ -9,14 +9,19 @@ app.config["SECRET_KEY"] = "git"
 bootstrap = Bootstrap(app)
 
 class SearchForm(FlaskForm):
-	query = StringField("Search", validators=[DataRequired(), Length(min=0,max=256)])
-	submit = SubmitField("Submit")
+	query = StringField("Search", validators=[DataRequired(), Length(min=0,max=256)], default="")
+	filter = SelectField(
+        "Filter",
+        choices=["Title", "Description"],
+        default="Title"
+    )
+	submit = SubmitField("Search")
 
 @app.route('/', methods=["GET", "POST"])
 def homePage():
 	form = SearchForm()
 	if form.validate_on_submit():
-		return render_template("searchResults.html", query=form.query.data)
+		return render_template("searchResults.html", query=form.query.data, filter=form.filter.data)
 	else:
 		return render_template("index.html", form=form)
 
