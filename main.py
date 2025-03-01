d9841fc6c08a0a43a1aa4c996c35a660e6c1ab53
Gregoire Saugnac <saugnacgj@cardiff.ac.uk>
2025-03-01 15:17:38 +0000

    Basic navbar working

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello():
-	return "Hello World!"
+	return render_template("index.html")
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=9000)
